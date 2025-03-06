57004d7c995a162065b4087626de734dc9e7a7a5
Vitaliy Obukh <Psiholog211@gmail.com>
2025-03-07 01:12:58 +0200

    jstestdom

---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+// const input = document.querySelector('#name-input');
+// const username = document.querySelector('#name-output');
+
+// const helloName = event => {
+//   username.textContent = event.currentTarget.value.trim();
+
+//   if (!username.textContent) {
+//     username.textContent = 'Anonymous';
+//   }
+// };
+
+// input.addEventListener('input', helloName);
